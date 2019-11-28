6caff530d11999592e607ff208debc728bc703ae
sei <teatime.0704@gmail.com>
2019-11-28 16:45:37 +0900

    [add] scss-setup

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+    <html lang='ja'>
+    <head>
+        <meta charset='UTF-8'>
+        <title>@yield('title') | POS</title>
+        <link rel='stylesheet' href='css/app.css'>
+        @yield('stylesheet')
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
---
+++ resources/views/top.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'TOP')
+
+@section('content')
+    <h1>TOP</h1>
+@endsection
